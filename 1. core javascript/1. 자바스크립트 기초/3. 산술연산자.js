037e1791f8a16c48c4400438f836860a9d3c91e2
junglimit <jaehan_jjh@naver.com>
2024-03-20 12:40:03 +0900

    #js 제어문

--- 1. core javascript/1. 자바스크립트 기초/3. 산술연산자.js
+++ 1. core javascript/1. 자바스크립트 기초/3. 산술연산자.js
@@ -14,3 +14,37 @@ console.log(0 % 5); // 몫과 나머지 전부 0
 //제곱 연산
 console.log(3 ** 4);
 
+console.log('========================');
+
+//증감 연산자
+var x = 3;
+x++;
+++x;
+x--;
+--x;
+console.log(`x:${x}`);
+
+
+// 전위 연산, 후위 연산
+// ++이 변수 앞에오면 = 보다 먼저 연산함
+var n1 = 10;
+var n2 = n1++;
+console.log(`n1:${n1}, n2:${n2}`);
+
+var n3 = 10;
+var n4 = ++n3;
+console.log(`n3:${n3}, n4:${n4}`);
+
+var xx = 5;
+xx++; //연산자가 1개라서 속도가 더 빠름
+xx += 1; // xx = xx + 1; 
+
+xx--;
+
+xx -= 1; // xx = xx - 1;
+
+xx *= 3;
+xx /= 5; 
+xx **= 2;
+xx %= 4;
+console.log(`xx:${xx}`);
