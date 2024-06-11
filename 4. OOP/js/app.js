ba972e6f2e73c721f55f57a14a9c4783c79c0f3c
junglimit <jaehan_jjh@naver.com>
2024-06-11 11:19:13 +0900

    # 코드 리팩터링

--- 4. OOP/js/app.js
+++ 4. OOP/js/app.js
@@ -1,41 +1,31 @@
 
-import  Product  from "./components/Product.js";
+import Product from "./components/Product.js";
+import ProductItem from "./components/ProductItem.js";
 
 // 상품 데이터
-const p1 = new Product('에어팟',200000, '에어팟으로 노래를 들어요.', 'https://www.apple.com/v/airpods/v/images/overview/airpods_3rd_gen__dhy5bknhvtqq_large.jpg' )
-const p2 = new Product('족발', 40000, '냠냠쩝쩝 족발 존맛.', 'https://i.namu.wiki/i/I63sEiy-8vUXVhV-I0IZiS9ntT0INuKXgBYAE3QqUvOlToSoEqSgpvEbUmxsFTXtoBRN4WJolyAFEAlDdeZFhQ.webp')
+const p1 = new Product('에어팟', 200000, '에어팟으로 노래를 들어요.', 'https://www.apple.com/v/airpods/v/images/overview/airpods_3rd_gen__dhy5bknhvtqq_large.jpg');
+const p2 = new Product('족발', 40000, '냠냠쩝쩝 족발은 앞다리야~', 'https://i.namu.wiki/i/I63sEiy-8vUXVhV-I0IZiS9ntT0INuKXgBYAE3QqUvOlToSoEqSgpvEbUmxsFTXtoBRN4WJolyAFEAlDdeZFhQ.webp');
 
+// 서버에서 전송된 JSON
 const productList = {
   products: [
-    p1,
-    p2,
-    new Product('요구르트', 500, '달디단요구르트', 'https://blisgo.com/wp-content/uploads/elementor/thumbs/%EC%9A%94%EA%B5%AC%EB%A5%B4%ED%8A%B8-%EB%B3%91-p5n5r18iqjwaho4l8dypfovtexj3hmml83x90jofls.jpg')
+    p1, 
+    p2, 
+    new Product('요구르트', 500, '요거어어어어어어르트', 'https://img-cf.kurly.com/shop/data/goods/1630556476801z0.jpg'),
+    new Product('쿼터파운더 치즈버거', 10000, '쿼팥치 먹고싶다', 'https://s7d1.scene7.com/is/image/mcdonalds/202201_3426-005_DoubleQuarterPounderwithCheese_832x472:1-3-product-tile-desktop?wid=765&hei=472&dpr=off'),
   ],
 
   render() {
     const $prodList = document.createElement('ul');
     $prodList.classList.add('product-list');
-    // 반복해서 li태그 생성
+    // 반복해서 li태그를 생성
     // console.log('products: ', this.products);
-    this.products.forEach(prod => {
-      const $prodLi = document.createElement('li')
-      $prodLi.classList.add('product-item');
-      $prodLi.innerHTML = `
-      <div>
-        <img src="${prod.image}" alt="${prod.title}">
-        <div class="product-item__content">
-          <h2>${prod.title}</h2>
-          <h3>${prod.price}원</h3>
-          <p>${prod.desc}</p>
-          <button>담기</button>
-        </div>
-      </div>
-    `;
-
-    $prodList.appendChild($prodLi);
-
+    this.products.forEach((prod) => {
+      const product = new ProductItem(prod);
+      $prodList.appendChild(product.render());
     });
-    // div#app에 ul 추가
+
+    // div#app에 ul추가
     document.getElementById('app').appendChild($prodList);
   },
 };
